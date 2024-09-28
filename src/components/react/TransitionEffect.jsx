8e7b4f2ee62e2cc035804801ba3edab7f146e136
Bryan <ryan.fabian244@gmail.com>
2024-09-27 20:37:35 -0500

    fix seo

--- src/components/react/TransitionEffect.jsx
+++ src/components/react/TransitionEffect.jsx
@@ -8,19 +8,19 @@ function TransitionEffect() {
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{duration:0.8, ease:"easeInOut"}}
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-yellow-300">
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-50 bg-yellow-300">
             </motion.div>
             <motion.div
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{delay:"0.2", duration:0.8, ease:"easeInOut"}}
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-gray-400">
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-gray-400">
             </motion.div>
                         <motion.div
                 initial={{ x: "100%", width: "100%" }}
                 animate={{ x: "0%", width: "0%" }}
                 transition={{delay:"0.4" ,duration:0.8, ease:"easeInOut"}}
-                className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-black">
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-black">
             </motion.div>
         </>
     )
