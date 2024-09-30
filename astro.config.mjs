258ef750f7dd90a3c5d64c31beb2d3ee17624111
Bryan <ryan.fabian244@gmail.com>
2024-09-29 20:21:02 -0500

    fix google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown"
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fremob.vercel.app/",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), react(), sitemap()]
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react(), sitemap(), partytown({config: {forward: ["dataLayer.push"]}})]
 }); 
